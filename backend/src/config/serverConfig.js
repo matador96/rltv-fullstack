5b7cf787c8ce880390dd092c15f0e0b4f7736cd2
matador96 <atubrah@mail.ru>
2023-04-04 21:13:23 +0400

    ok

--- backend/src/config/serverConfig.js
+++ backend/src/config/serverConfig.js
@@ -1,11 +1,12 @@
 const mode = process.env.NODE_ENV || "development";
+require("dotenv").config();
 
 const config = {
   development: {
     port: "3003",
   },
   production: {
-    port: process.env.SERVER_PORT || 8080,
+    port: process.env.SERVER_PORT || 3044,
   },
 };
 
